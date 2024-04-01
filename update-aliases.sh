a27cae1fe36eb02dae2497efff638ddc8e7b9fed
lucaohost <lucas.reginatto.de.lima@gmail.com>
2024-04-01 17:56:06 -0300

    Add environment codes

---
+++ update-aliases.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+folder_before_script=$(pwd)
+
+cd /mnt/c/git/environment
+
+echo "# Paste the code below in your ~/.bashrc" > aliases.sh
+
+grep -A 1000000 -E ' My\s+Aliases' ~/.bashrc | tail -n +2 >> aliases.sh
+
+git add aliases.sh > /dev/null 2>&1
+
+git commit -m "Update aliases" > /dev/null 2>&1
+
+git push origin master  > /dev/null 2>&1
+
+cd "$folder_before_script" > /dev/null 2>&1
