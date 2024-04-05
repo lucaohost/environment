a1f848d179d2433580e30ae7af0286301142a51e
lucaohost <lucas.reginatto.de.lima@gmail.com>
2024-04-05 09:25:14 -0300

    Update alias updater

--- update-aliases.sh
+++ update-aliases.sh
@@ -3,10 +3,6 @@ folder_before_script=$(pwd)
 
 cd /mnt/c/git/environment
 
-echo -e "# Paste the code below at the end of your ~/.bashrc\n# My Aliases" > aliases.sh
-
-grep -A 1000000 -E ' My\s+Aliases' ~/.bashrc | tail -n +2 >> aliases.sh
-
 git add aliases.sh > /dev/null 2>&1
 
 git commit -m "Update aliases" > /dev/null 2>&1
