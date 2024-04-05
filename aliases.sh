b7e53d79922305854f0b0bf388ad221990584d3c
lucaohost <lucas.reginatto.de.lima@gmail.com>
2024-04-05 09:42:18 -0300

    Update aliases

--- aliases.sh
+++ aliases.sh
@@ -1,29 +1,23 @@
-# Paste the line below without # in your ~/.bashrc
-# source /mnt/c/git/environment/aliases.sh
-
-# My Aliases
-
-# c = Codes (Go directory with all Codes of Github Projects)
+# c   = Codes (Go directory with all Codes of Github Projects)
 alias c='cd /mnt/c/git/'
 
 # rmb = Remember my Bashes
 alias rmb='ls -1 /mnt/c/git/environment/*.sh'
 
 # rma = Remeber my Aliases
-alias rma="grep -A 1000000 -E ' My\s+Aliases' ~/.bashrc | tail -n +2"
+alias rma="grep -E '^#' /mnt/c/git/environment/aliases.sh"
 
-# x = Exit (Close Terminal)
+# x   = Exit (Close Terminal)
 alias x="exit"
 
-# xx = Double Exit (Turn off the Computer)
+# xx  = Double Exit (Turn off the Computer)
 if uname -a | grep -q "microsoft\|WSL"; then
     alias xx="wsl.exe -d ubuntu -- powershell.exe Stop-Computer"
 elif uname -a | grep -q "Ubuntu"; then
     alias xx="poweroff"
 fi
 
-# xs = Exit Sleep (Turn off the computer after N minutes)
-# Usage: xs 45
+# xs  = Exit Sleep (Turn off the computer after N minutes) | Usage: xs 45
 xs() {
     if grep -qE "(microsoft|WSL)" /proc/version &>/dev/null && [[ "$(uname -r)" != *Microsoft* ]]; then
         xs_windows "$@"
@@ -50,12 +44,18 @@ xs_ubuntu() {
     sudo systemctl suspend
 }
 
-# r = Reboot the computer
+# r   = Reboot the computer
 if uname -a | grep -q "Microsoft\|WSL"; then
     alias r="wsl.exe -d ubuntu -- powershell.exe Restart-Computer"
 elif uname -a | grep -q "Ubuntu"; then
     alias r="reboot"
 fi
 
-# ua = Update Aliases
+# ua  = Update Aliases
 alias ua="/mnt/c/git/environment/update-aliases.sh"
+
+# ea  = Edit Aliases
+alias ea="nano /mnt/c/git/environment/aliases.sh"
+
+# eb  = Edit ~/.bashrc
+alias eb="nano ~/.bashrc"
