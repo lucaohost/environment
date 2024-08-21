f509be4694d72dc93347d03c868d30ea1db7e23e
Lucas Reginatto de Lima <lucas.reginatto.de.lima@gmail.com>
2024-08-21 17:59:43 -0300

    Add gcam alias

--- scripts/git-commit-m.sh
+++ scripts/git-commit-m.sh
@@ -1,11 +1,8 @@
-gu() {
+gcam() {
     local commit_msg=""
     for param in "$@"; do
         commit_msg="$commit_msg$param "
     done
     commit_msg="${commit_msg%?}" #remove last white space
-    git add -A
     git commit -m "$commit_msg"
-    git push --force
-    echo "$commit_msg"
 }
