7fd1d6426cdab4b03c34618b1ae3afab15dc9805
Lucas Reginatto de Lima <lucas.reginatto.de.lima@gmail.com>
2024-08-27 19:38:50 -0300

    Add Window's Downloads alias

--- aliases.sh
+++ aliases.sh
@@ -116,3 +116,6 @@ export DONT_PROMPT_WSL_INSTALL=true
 
 # intj  = open InteliJ
 alias intj='(intellij-idea-community > /dev/null 2>&1 &)'
+
+# wind  = Windows's Downloads
+alias wind='cd /mnt/c/Users/lucas/Downloads'
