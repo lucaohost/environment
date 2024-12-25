e1c44ada898ca73dff571ebaaf8a8621afda4360
Lucas Reginatto de Lima <lucas.reginatto.de.lima@gmail.com>
2024-12-25 02:41:00 -0300

    fix: Update date format in commit info extraction

--- scripts/github-update.sh
+++ scripts/github-update.sh
@@ -10,7 +10,7 @@ gu() {
 
 update_git_work_commits() {
     local commit_msg="$1"
-    commit_info=$(git log --author="lucas.reginatto@ifood.com.br" --pretty=format:"%h|%ae|%ad|$(git symbolic-ref --short HEAD)|%s|$(basename $(git rev-parse --show-toplevel))" --abbrev=8 --date=iso -n 1)
+    commit_info=$(git log --author="lucas.reginatto@ifood.com.br" --pretty=format:"%h|%ae|%ad|$(git symbolic-ref --short HEAD)|%s|$(basename $(git rev-parse --show-toplevel))" --abbrev=8 --date=format:'%Y-%m-%dT%H:%M:%S-03:00' -n 1)
     echo $commit_info >> $HOME/git/git-work-commits/git-work-commits-data.txt
 
     # Extract commit info
