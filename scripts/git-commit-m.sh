da47526b369b392d9784164fbf05d9ebc9b18d96
Lucas Reginatto de Lima <lucas.reginatto.de.lima@gmail.com>
2025-01-09 18:07:37 -0300

    feat: Ensure correct email commiting

--- scripts/git-commit-m.sh
+++ scripts/git-commit-m.sh
@@ -4,5 +4,18 @@ gcam() {
         commit_msg="$commit_msg$param "
     done
     commit_msg="${commit_msg%?}" #remove last white space
+    local repo_name=$(basename "$(git rev-parse --show-toplevel)")
+
+    if [[ -z "$PERSONAL_EMAIL" || -z "$PROFESSIONAL_EMAIL" ]]; then
+        echo "Warning: PERSONAL_EMAIL and/or PROFESSIONAL_EMAIL environment variables are not set."
+        return 1
+    fi
+
+    if [[ "$repo_name" == "environment" || "$repo_name" == "my-codes" || "$repo_name" == "git-work-commits" ]]; then
+        git config user.email "$PERSONAL_EMAIL"
+    fi
+
     git commit -m "$(printf "%b" "$commit_msg")"  # Interpreta o \n como nova linha
+
+    git config user.email "$PROFESSIONAL_EMAIL"
 }
